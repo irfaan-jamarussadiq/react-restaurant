cc682d99c80b497d56f1199ca924d3cf16213618
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-12 14:40:11 -0500

    Add basic style to the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,13 @@ import { Link, Outlet } from 'react-router-dom'
 
 function NavBar() {
     return (
-        <div>
+        <div className="nav-container">
             <nav>
-                <ul>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/menu">Menu</Link>
-                    <Link to="/contact">Contact</Link>                                
+                <ul className="nav-links">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                    <Link className="nav-link" to="/menu">Menu</Link>
+                    <Link className="nav-link" to="/contact">Contact</Link>                                
                 </ul>
             </nav>
             <Outlet />
