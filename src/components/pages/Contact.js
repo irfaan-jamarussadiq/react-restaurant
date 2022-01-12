df90632c76c9f940367356701dafe7209e04afb7
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-12 18:39:35 -0500

    Add basic HTML/CSS for contact form page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,32 @@
+// import Form from '../Form.js'
+
+function Form() {
+    return (
+        <form>
+            { /* ... */ }
+        </form>
+    )
+}
+
 function Contact() {
     return (
-        <div>
+        <div className="form-container shadow-lg">
             <h2>Contact</h2>
+            <form className="form-fields">
+                <div className="form-field"> 
+                    <label>Email</label>
+                    <input className="form-input"/>
+                </div>
+                <div className="form-field">
+                    <label>Subject</label>
+                    <input className="form-input"/>
+                </div>
+                <div className="form-field">
+                    <label>Message</label>
+                    <textarea rows="4" className="form-input"></textarea>
+                </div>
+                <button type="submit" className="submit-btn">Submit</button>
+            </form>
         </div>
     )
 }
