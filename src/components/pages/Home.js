b738d32e1bb9572cf867a21c20a52dd343605467
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-17 21:47:53 -0500

    Redesign home page
    Add restaurant logo and picture with expected restaurant food to home
    page. This is in line with other restaurant websites found on the web,
    the idea being to pull customers in immediately by showing the food on
    the home page instead of only later in the menu.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,8 +2,14 @@ import home from '../../images/home.jpg'
 
 function Home() {
     return (
-        <div className="home-page page-container">
-            <img src={home} alt="South Indian food"/>
+        <div className="home-page img-wrapper">
+            <div className="img-overlay">
+                <div className="heading-text">
+                    <h1 id="title">Latheeth</h1>
+                    <h2 id="subtitle">Indian Vegetarian Restaurant</h2>
+                </div>
+            </div>
+            <img src={home} alt="Indian vegetarian food"/>
         </div>
     )
 }
