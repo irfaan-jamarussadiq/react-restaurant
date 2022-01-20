ef1e41a5000c253de92427df2c7bfaca8af63856
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-19 21:52:52 -0500

    Add smooth scrolling from home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,18 @@ import home from '../../images/home.jpg'
 import { Link } from 'react-router-dom'
 
 function Home() {
+    /* Consider having this in page component! */
+    const onClick = () => {
+        window.scrollBy({top: 1.2*window.innerHeight, behavior: 'smooth'})
+    }
+
     return (
         <div className="home-page img-wrapper">
             <div className="img-overlay">
                 <div className="heading-text">
                     <h1 id="title">Latheeth</h1>
                     <h2 id="subtitle">Indian Vegetarian Restaurant</h2>
-                    <Link to="/">
+                    <Link to="/" onClick={onClick}>
                         <div className="arrow" />
                     </Link>
                 </div>
