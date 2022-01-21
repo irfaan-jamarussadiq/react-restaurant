03fe28f05268a305dc402476247f7d32da5db247
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-21 18:50:09 -0500

    Replace background image for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,15 +5,17 @@ function Home() {
         <div className="home-page img-wrapper" id="home">
             <div className="img-overlay">
                 <div className="header-container">
-                    <h1>Latheeth</h1>
-                    <p className="address">12345 Example Street</p>
-                    <p className="address">City, State 12345</p>
+                    <div className="title-container shadow-xl">
+                        <h1>Latheeth</h1>
+                        <p className="address">12345 Example Street</p>
+                        <p className="address">City, State 12345</p>
+                    </div>
                     <Link to="about" smooth={true} offset={-70}>
                         <div className="arrow" />
                     </Link>
                 </div>
             </div>
-            <img src="images/home.jpg" alt="Indian vegetarian food"/>
+            <img src="images/sadhya.jpg" alt="Indian vegetarian food"/>
         </div>
     )
 }
