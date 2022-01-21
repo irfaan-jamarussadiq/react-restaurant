e2794fbd9667b010b5bde5c424c49f55aea3289e
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-01-21 16:20:20 -0500

    Add all files for successful netlify build

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import './App.css'
-import { Routes, Route, Outlet } from 'react-router-dom'
 import { NavBar, Footer, Home, About, HoursOfOperation, Menu, Contact } from './index'
-import RestaurantInfo from './components/RestaurantInfo'
-import logo from './images/logo.png'
-
-function Header({ links, logo }) {
-  return (
-    <header className="App-header shadow-lg">
-      <RestaurantInfo 
-        streetAddress='12345 Example Street' 
-        city='City' 
-        state='ST' 
-        zipcode='99999'
-        phone='999-999-9999' 
-      />
-      <NavBar links={links}/>
-    </header>
-  )
-}
 
 function App() {
-  const pages = ['About', 'Hours', 'Menu', 'Contact']
-
   return (
     <div className="App">
-      <Header links={pages} logo={logo} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/hours" element={<HoursOfOperation />} />  
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<Contact />} />        
-      </Routes>
-      <Outlet />
-      {/* <About />
+      <NavBar links={['About', 'Hours', 'Menu', 'Contact']}/>
+      <Home />
+      <About />
       <HoursOfOperation />
       <Menu />
-      <Contact /> */}
+      <Contact />
       <Footer year='2022' company='Latheeth' developer='Irfaan Jamarussadiq' />
     </div>
   )
