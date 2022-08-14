725ebba42d9fcf2e995601d7a46bacd851e6040d
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-08-14 16:17:31 -0400

    Add build script for app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
