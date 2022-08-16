da7127320f1de4eda3b46ea1f2b26089b2e45510
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-08-15 23:08:32 -0400

    Update Order View

--- client/src/components/pages/Order.js
+++ client/src/components/pages/Order.js
@@ -4,6 +4,7 @@ function Order() {
     return (
         <form id="order" className="page-container">
             <h2>Order</h2>
+            <p>View Our Menu</p>
             <Link to="order" target="_blank" rel="noopener noreferrer">
                 Order Now
             </Link>
