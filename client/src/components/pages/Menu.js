f1ee933a743156ddcc57d4a1aec90815ea08fc8a
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-11-27 12:11:26 -0500

    Refactor order page into smaller components

--- client/src/components/pages/Menu.js
+++ client/src/components/pages/Menu.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useState } from "react"
 import Items from "../../items.json"
 
 function Menu() {
     return (
         <div className="page-container" id="order-page">
             <h1 className="menu-title">Order Now</h1>
-            <OrderSection sectionName="Appetizers" />             
+            <OrderSection sectionName="Appetizers" />
             <OrderSection sectionName="Breakfast" />
-            <OrderSection sectionName="Lunch Entrées" />                       
+            <OrderSection sectionName="Lunch Entrées" />
+            <OrderTotal />
             <button type="submit" id="submit-order" className="order-button">Submit Order</button>
         </div>
+    )
+}
+
+function OrderTotal() {
+    const [total, setTotal] = useState(0)
+    return (
+        <p>Order Total: ${total.toFixed(2)}</p>
     )
 }
 
@@ -25,34 +33,43 @@ function OrderSection({ sectionName }) {
     )
 }
 
-function OrderItem({ item }) {
+function ItemDescription({ item }) {
+    return (
+        <>
+            <p className="item-name">{item.name}</p>
+            <p className="item-price">{`$${item.price.toFixed(2)}`}</p>
+            <p>{item.description || ""}</p>
+        </>
+    )
+}
 
+function OrderButton() {
     const [button, setButton] = useState({
         ordered: false,
         className: "order-button default",
         text: "Add to order"
-    });
-
+    })
     const orderItem = () => setButton({
         ordered: !button.ordered,
         className: "order-button " + (button.ordered ? "default" : "selected"),
         text: button.ordered ? "Add to order" : "Remove from order"
-    });
+    })
+    return (
+        <button className={button.className} onClick={orderItem}>
+            {button.text}
+        </button>
+    )
+}
 
+function OrderItem({ item }) {
     return (
         <div className="order-card shadow-lg">
-            <div>
-                <img src={`images/${item.image}`} alt={item.name} />
-            </div>
+            <img src={`images/${item.image}`} alt={item.name} />
             <div className="order-content">
-                <div className="item-name">{item.name}</div>
-                <div className="item-price">{`$${item.price.toFixed(2)}`}</div>                    
-                <div>{item.description || ""}</div>
+                <ItemDescription item={item} />
                 <div className="order-selection">
                     <OrderQuantity options={[1, 2, 3, 4, 5]} />
-                    <button className={button.className} onClick={orderItem}>
-                        {button.text}
-                    </button>
+                    <OrderButton />
                 </div>
             </div>
         </div>
@@ -60,11 +77,13 @@ function OrderItem({ item }) {
 }
 
 function OrderQuantity({ options }) {
+    const [quantity, setQuantity] = useState(options[0])
+    const changeQuantity = event => setQuantity(event.target.value)
     return (
         <div className="order-quantity-container">
-            <label>Quantity</label>
-            <select className="order-quantity">
-                {options.map(option => <option key={option}>{option}</option>)}
+            <label>Quantity: {quantity}</label>
+            <select className="order-quantity" value={quantity} onChange={changeQuantity}>
+                {options.map(option => <option key={option} value={option}>{option}</option>)}
             </select>
         </div>
     )
