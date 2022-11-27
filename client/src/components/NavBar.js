7b6812e0d09179ce19852889b8ea917a11f350f1
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-11-27 13:27:41 -0500

    Refactor NavBar component to avoid preoptimization

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,40 +1,34 @@
 import { Link } from 'react-scroll'
 import { Link as RouterLink } from 'react-router-dom'
 
-function NavBar({ links }) {
+function NavBar() {
     return (
-        <div className="nav-container shadow-xl">
-            <nav>
-                <div className="nav-links">
-                    {links.map(link => {
-                        if (link === "Order") {
-                            return (
-                                <RouterLink                                     
-                                    className="nav-link" 
-                                    key={link} 
-                                    to={link.toLowerCase()}
-                                    target="_blank" rel="noopener noreferrer"
-                                >
-                                    {link}
-                                </RouterLink>
-                            )
-                        } else {
-                            return (
-                                <Link 
-                                    className="nav-link" 
-                                    key={link} 
-                                    to={link.toLowerCase()}
-                                    smooth={true}
-                                    offset={-70}
-                                >
-                                    {link}
-                                </Link>
-                            )
-                        }
-                    })}                             
-                </div>
-            </nav>
-        </div>
+        <nav className="nav-container shadow-xl">
+            <NavLink link="about" text="About" />
+            <NavLink link="hours" text="Hours" />
+            <RouterLink 
+                className="nav-link" 
+                to="order" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                Order
+            </RouterLink>
+            <NavLink className="nav-link" link="contact" text="Contact" />                        
+        </nav>
+    )
+}
+
+function NavLink({link, text}) {
+    return (
+        <Link 
+            className="nav-link" 
+            to={link}
+            smooth={true} 
+            offset={-70}
+        >
+            {text}
+        </Link>
     )
 }
 
