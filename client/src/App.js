9fc98659f2ae8e5463489d4a7dc8c3396ee81468
Irfaan Jamarussadiq <irfaanj@umd.edu>
2023-09-26 18:43:19 -0400

    Create Latheeth website main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,52 @@
 import './App.css'
-import {
-  NavBar,
-  Footer,
-  LandingPage,
-  About, 
-  HoursOfOperation, 
-  Contact
-} from './index'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <LandingPage />
-      <About />
-      <HoursOfOperation />
-      <Contact />
-      <Footer />
+      <div id="main-page">
+        <h1 id="website-title">Latheeth</h1>
+        <h3>Experience authentic vegetarian Indian cuisine</h3>
+        <div className="main-page-img-container">
+          <img src="images/sadhya.jpg" alt="Indian vegetarian meal with rice and curry set" />
+        </div>
+      </div>
+      <div className="menu-preview">
+        <h2 id="menu-heading">Our Menu</h2>
+        <div className="menu-preview-sections">
+          <section className="preview-section">
+            <button className="preview-section-btn">
+              <h2>Breakfast Items</h2>
+            </button>
+            <div className="menu-img-container">
+              <img className="menu-img" src="images/mysore dosa.jpg" alt="Table with Indian sweets" />
+            </div>
+          </section>
+          <section className="preview-section">
+            <button className="preview-section-btn">
+              <h2>Main Dishes</h2>
+            </button>
+            <div className="menu-img-container">
+              <img className="menu-img" src="images/sadhya.jpg" alt="Leaf with rice and Indian curries" />
+            </div>
+          </section>
+          <section className="preview-section">
+            <button className="preview-section-btn">
+              <h2>Appetizers</h2>
+            </button>
+            <div className="menu-img-container">
+              <img className="menu-img" src="images/samosa.jpg" alt="Samosas" />
+            </div>
+          </section>
+          <section className="preview-section">
+            <button className="preview-section-btn">
+              <h2>Desserts</h2>
+            </button>
+            <div className="menu-img-container">
+              <img className="menu-img" src="images/desserts.jpg" alt="Table with Indian sweets" />
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   )
 }
