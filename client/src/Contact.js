8ca59062186cc07426b88c7fd1a181d8ef4a0b59
Irfaan Jamarussadiq <irfaanj@umd.edu>
2023-10-26 18:36:47 -0400

    Move colors to variables and adjust Contact styles

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,23 +1,26 @@
+import Page from "./Page"
+
 function Contact() {
   return (
-    <form className="contact-form page">
-      <h2>Contact Us</h2>
-      <div className="form-group">
-        <label>Name</label>
-        <input required />
-      </div>
-      <div className="form-group">
-        <label>Email</label>
-        <input type="email" required />
-      </div>
-      <div className="form-group">
-        <label>Message</label>
-        <textarea className="contact-message" required />
-      </div>
-      <button className="contact-submit" type="submit">
-        Send message
-      </button>
-    </form>
+    <Page title="Contact Us">
+      <form className="contact-form">
+        <div className="form-group">
+          <label>Name</label>
+          <input required />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" required />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea className="contact-message" required />
+        </div>
+        <button className="btn" type="submit">
+          Send message
+        </button>
+      </form>
+    </Page>
   )
 }
 
